374fb9c6472a75052e76d13590a8283c77d85d9a
boros.tamas <boros.tamas456@gmail.com>
2025-05-17 23:20:07 +0200

    add products automatically

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO products (product_name, price, type) VALUES ('Black Coffee', 4, 'drink');
+INSERT INTO products (product_name, price, type) VALUES ('Latte', 5, 'drink');
+INSERT INTO products (product_name, price, type) VALUES ('Mocha', 6, 'drink');
+INSERT INTO products (product_name, price, type) VALUES ('Tea', 3, 'drink');
+INSERT INTO products (product_name, price, type) VALUES ('Milk', 2, 'topping');
+INSERT INTO products (product_name, price, type) VALUES ('Hazelnut Syrup', 3, 'topping');
+INSERT INTO products (product_name, price, type) VALUES ('Chocolate Sauce', 5, 'topping');
+INSERT INTO products (product_name, price, type) VALUES ('Lemon', 2, 'topping');
